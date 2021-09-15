8c496ef0e7a2265b9b9a6c069267e0ff3a26a113
Bunbee <barnabmutinhiri@gmail.com>
2021-09-15 22:28:56 +0200

    Fix linter errors

--- script.js
+++ script.js
@@ -4,43 +4,43 @@ const closeBtn = document.querySelector('.close');
 const speakersList = document.querySelectorAll('.f-speaker');
 
 const toggle = () => {
-    navBar.classList.toggle('d-none');
-    navBar.classList.toggle('show');
-    menu.classList.toggle('d-none');
-    closeBtn.classList.toggle('d-none');
+  navBar.classList.toggle('d-none');
+  navBar.classList.toggle('show');
+  menu.classList.toggle('d-none');
+  closeBtn.classList.toggle('d-none');
 };
 
 menu.addEventListener('click', toggle);
 closeBtn.addEventListener('click', toggle);
 
 const speakers = [
-    {
-        mutauri: 'John Phiri',
-        title: 'Engineer at We can build anything',
-        background: ' Community advocate for the people in their endeavours',
-        img: 'img/engineer.jpeg'
-    },
+  {
+    mutauri: 'John Phiri',
+    title: 'Engineer at We can build anything',
+    background: ' Community advocate for the people in their endeavours',
+    img: 'img/engineer.jpeg',
+  },
 
-    {
-        mutauri: 'Lesbon Mabasa',
-        title: 'Doctor at Murapo We can surgion anything',
-        background: ' Community advocate for the people in their endeavours',
-        img: 'img/doctor.jpeg'
-    },
+  {
+    mutauri: 'Lesbon Mabasa',
+    title: 'Doctor at Murapo We can surgion anything',
+    background: ' Community advocate for the people in their endeavours',
+    img: 'img/doctor.jpeg',
+  },
 
-    {
-        mutauri: 'Tarisai Homwe',
-        title: 'Developer  at We can craete anything',
-        background: ' Community advocate for the people in their endeavours',
-        img: 'img/software.jpeg'
-    },
+  {
+    mutauri: 'Tarisai Homwe',
+    title: 'Developer  at We can craete anything',
+    background: ' Community advocate for the people in their endeavours',
+    img: 'img/software.jpeg',
+  },
 
-    {
-        mutauri: 'Changara Chinx',
-        title: 'Farmer at Varimi vanhasi Corporates for Feeding the country',
-        background: ' Community advocate for the people in their endeavours',
-        img: 'img/farmer.jpeg'
-    },
+  {
+    mutauri: 'Changara Chinx',
+    title: 'Farmer at Varimi vanhasi Corporates for Feeding the country',
+    background: ' Community advocate for the people in their endeavours',
+    img: 'img/farmer.jpeg',
+  },
 
 ];
 
@@ -56,5 +56,4 @@ const imgs = (() => {
     collabInfoNodes[1].innerHTML = speakers[eachCollab].mutauri;
     collabInfoNodes[3].innerHTML = speakers[eachCollab].title;
     collabInfoNodes[5].innerHTML = speakers[eachCollab].background;
-  }
-  
+}
