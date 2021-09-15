2fd04cbdd4b8df5828006f311a5c4ee239faad77
Bunbee <barnabmutinhiri@gmail.com>
2021-09-15 14:59:53 +0200

    Add dropdown functionality

---
+++ script.js
@@ -0,0 +1,13 @@
+const navBar = document.querySelector('#navbarNav')
+const menu = document.querySelector('.hamburger')
+const closeBtn = document.querySelector('.close')
+
+const toggle = () => {
+    navBar.classList.toggle('d-none');
+    navBar.classList.toggle('show');
+    menu.classList.toggle('d-none');
+    closeBtn.classList.toggle('d-none');
+};
+
+menu.addEventListener('click', toggle);
+closeBtn.addEventListener('click', toggle);
